2f8e583f5e2e3fb1180075b2a1c8bad86951c39f
ryeung <raymond.yeung@outlook.com.au>
2024-08-18 05:49:49 +1000

    Initial commit

---
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,42 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  'sqwishy/vim-sqf-syntax',
+  'mbbill/undotree',
+  'tpope/vim-fugitive',
+  'nvim-telescope/telescope-project.nvim',
+  'dense-analysis/ale',
+  'jonpas/vim-sqflint-ale',
+  {
+    'smoka7/hop.nvim',
+    version = '*',
+    opts = {},
+  },
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    -- branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons',
+      'MunifTanjim/nui.nvim',
+    },
+  },
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    config = true,
+    -- use opts = {} for passing setup options
+    -- this is equalent to setup({}) function
+  },
+  {
+    "swaits/scratch.nvim",
+    lazy = true,
+    cmd = {
+      "Scratch",
+      "ScratchSplit",
+    },
+    opts = {},
+  };
+}
