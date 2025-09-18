f7fb62f5eebf769fedfab36d2390528c2fb87f83
rhyeung <raymond.yeung@outlook.com.au>
2025-09-18 21:37:02 +1000

    Updated to nvim v0.11.4

--- .config/nvim/ftplugin/cc.lua
+++
@@ -1,5 +0,0 @@
-local o = vim.o
-o.expandtab = true -- expand tab input with spaces characters
-o.smartindent = true -- syntax aware indentations for newline inserts
-o.tabstop = 2 -- num of space characters per tab
-o.shiftwidth = 2 -- spaces per indentation level
